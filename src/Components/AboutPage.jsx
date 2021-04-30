4001ad1a742682eae7c052caa88e8ece4b3e43ae
‘Shelby <sls1881@gmail.com>
2021-04-29 17:44:48 -0700

    Header and CSS

--- src/Components/AboutPage.jsx
+++ src/Components/AboutPage.jsx
@@ -7,16 +7,14 @@ export default class AboutPage extends Component {
         <div className="Cherno">
           <h3>Cherno Badjan</h3>
           <img
+            className="about-img"
             src="https://media-exp1.licdn.com/dms/image/C5603AQEAKZIZBv2FtA/profile-displayphoto-shrink_800_800/0/1612209733545?e=1625097600&v=beta&t=VobXYJbP7ujewdr0EdbSHnbBR8VvV9OxtKKmQBQtFSI"
             alt="Cherno, Software Engineer"
           />
           <p>
-            Cherno Badjan is a solution-driven Software Engineer who is
-            passionate about developing easy-to-navigate websites. He focuses on
-            clean, organized code to prevent defects and create a more seamless
-            experience for the end user. He enjoys traveling to exotic beaches
-            and soaking up the sun. A rather brilliant software engineer with a
-            passion for making the complicated simple.
+            Cherno Badjan is a Software Engineer with a passion for making the
+            complicated simple. A rather brilliant team mate with a high
+            cultural IQ thanks to his travels around the world.
           </p>
           <div className="profile">
             <a href="https://www.linkedin.com/in/chernobadjan/">
@@ -38,16 +36,15 @@ export default class AboutPage extends Component {
         <div className="Josie">
           <h3>Josie Cantu</h3>
           <img
+            className="about-img"
             src="https://media-exp1.licdn.com/dms/image/C5603AQHSNGXZIlzjNQ/profile-displayphoto-shrink_400_400/0/1610300308279?e=1625097600&v=beta&t=1pb9s5zlF02AMBr0SBx3ZcdN2Oy4bf38bHYxxGVn97o
 "
             alt="Josie, Software Engineer"
           />
           <p>
-            Josie Cantu is a solution-driven Software Engineer who is passionate
-            about developing easy-to-navigate websites. She focuses on clean,
-            organized code to prevent defects and create a more seamless
-            experience for the end user. She enjoys traveling to exotic beaches
-            and soaking up the sun.
+            Josie Cantu is a Software Engineer who is driven by her curiosity
+            and desire to develop her skills. She thrives in a team setting and
+            loves mob programming. She is a dog lover and dog mom to Noey.
           </p>
           <div className="profile">
             <a href="https://www.linkedin.com/in/josiemcantu">
@@ -69,16 +66,15 @@ export default class AboutPage extends Component {
         <div className="Lori">
           <h3>Lori Winston</h3>
           <img
+            className="about-img"
             src="https://media-exp1.licdn.com/dms/image/C5603AQFoupwWak_ygA/profile-displayphoto-shrink_800_800/0/1609720117211?e=1625097600&v=beta&t=fvs9lkson3GtfPh86igm-0lGcWVRNUu-8JsxpFax57A
 "
             alt="Lori, Software Engineer"
           />
           <p>
-            Lori Winston is a solution-driven Software Engineer who is
-            passionate about developing easy-to-navigate websites. She focuses
-            on clean, organized code to prevent defects and create a more
-            seamless experience for the end user. She enjoys traveling to exotic
-            beaches and soaking up the sun
+            Lori Winston is a Software Engineer that loves creative
+            problem-solving, working in a team, and any reason to get outside in
+            the PNW.
           </p>
           <div className="profile">
             <a href="https://www.linkedin.com/in/loriwinston/">
@@ -100,6 +96,7 @@ export default class AboutPage extends Component {
         <div className="shelby">
           <h3>Shelby Strouse</h3>
           <img
+            className="about-img"
             src="https://media-exp1.licdn.com/dms/image/C5603AQFVWPTD37oZjw/profile-displayphoto-shrink_200_200/0/1619719045782?e=1625097600&v=beta&t=-rh1AS0GkdX5gBaIcYQPj7ecOt5NA_KMYAFEhRnCpUc
             "
             alt="Shelby, Software Engineer"
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,28 +1,32 @@
 import React, { Component } from "react";
 import { NavLink, withRouter } from "react-router-dom";
+import logo from "../asset/full-logo.jpg";
 
 export default withRouter(
   class Header extends Component {
     render() {
       return (
-        <nav className="navContainer">
-          {this.props.location.pathname !== "/" && (
-            <NavLink className="nav-link" exact activeClassName="link" to="/">
-              Search
-            </NavLink>
-          )}
+        <div>
+          <nav className="navContainer">
+            {this.props.location.pathname !== "/" && (
+              <NavLink className="nav-link" exact activeClassName="link" to="/">
+                Search
+              </NavLink>
+            )}
 
-          {this.props.location.pathname !== "/about" && (
-            <NavLink
-              className="nav-link"
-              exact
-              activeClassName="link"
-              to="/about"
-            >
-              About Us
-            </NavLink>
-          )}
-        </nav>
+            {this.props.location.pathname !== "/about" && (
+              <NavLink
+                className="nav-link"
+                exact
+                activeClassName="link"
+                to="/about"
+              >
+                About Us
+              </NavLink>
+            )}
+            <img className="logo" src={logo} alt="destinare-logo" />
+          </nav>
+        </div>
       );
     }
   }
